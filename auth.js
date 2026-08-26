b00a22fd46a240bb6f918fe67cd2c9bd3bb8f163
eslamhamadi779-sys <eslamhamadi779@gmail.com>
2026-08-25 20:07:33 -0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,12 @@
+const SUPABASE_URL = "https://aigxyjtkxmqetucmctaf.supabase.co";       // مثال: https://xxxxxxxx.supabase.co
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpZ3h5anRreG1xZXR1Y21jdGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU5Njc0MTgsImV4cCI6MjEwMTU0MzQxOH0.WVwOI-wyOFRfBgOgMdRCTn8E9pToy3GzMW_p0Thb5cc";  // مفتاح anon/public من إعدادات المشروع
+
+
+let supabaseClient = null;
+if (SUPABASE_URL && SUPABASE_ANON_KEY && window.supabase) {
+    supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+}
+
 
 // 1. عرف Supabase اول حاجة
 const SUPABASE_URL = 'https://aigxyjtkxmqetucmctaf.supabase.co'
@@ -10,7 +19,7 @@ async function signInWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
     options: {
-      redirectTo: 'https://eslamhamadi779-sys.github.io/test-fx/' // <-- السطر اللي كان ناقص
+      redirectTo: 'https://eslamhamadi779-sys.github.io/test-fx/' // <-- 
     }
   })
   
